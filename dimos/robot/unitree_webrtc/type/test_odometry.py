bfec90a5be6e0d8b6d703039dad53e98b35083b7
lesh <lesh@sysphere.org>
2025-05-30 16:59:18 +0300

    odometry stream test

--- dimos/robot/unitree_webrtc/type/test_odometry.py
+++ dimos/robot/unitree_webrtc/type/test_odometry.py
@@ -1,10 +1,48 @@
 import pytest
 from dimos.robot.unitree_webrtc.type.odometry import Odometry
 from dimos.robot.unitree_webrtc.testing.multimock import Multimock
+from dimos.utils.testing import SensorReplay
 
 
-@pytest.mark.needsdata
-def test_odometry_time():
-    (timestamp, odom_raw) = Multimock("athens_odom").load_one(33)
-    print("RAW MSG", odom_raw)
-    print(Odometry.from_msg(odom_raw))
+@pytest.mark.tool
+def test_store_odometry_stream():
+    import os
+    import threading
+    import time
+    from dotenv import load_dotenv
+    from dimos.robot.unitree_webrtc.unitree_go2 import UnitreeGo2
+    from dimos.utils.testing import SensorStorage
+
+    load_dotenv()
+
+    robot = UnitreeGo2(ip=os.getenv("ROBOT_IP"), mode="ai")
+    robot.standup()
+
+    def streamlogger(element):
+        print(element)
+        return element
+
+    SensorStorage("raw_odometry_rotate_walk").save_stream(robot.raw_odom_stream()).subscribe(print)
+
+    shutdown_event = threading.Event()
+    print("Robot is running. Press Ctrl+C to stop.")
+
+    try:
+        # Keep the main thread alive while the robot loop runs in the background
+        while not shutdown_event.is_set():
+            time.sleep(0.1)
+    except KeyboardInterrupt:
+        print("\nKeyboard interrupt received. Shutting down...")
+    finally:
+        print("Stopping robot...")
+        robot.liedown()
+        print("Robot stopped.")
+
+
+def test_odometry_stream():
+    counter = 0
+    for message in SensorReplay(
+        name="raw_odometry_rotate_walk", autocast=lambda x: Odometry.from_msg(x)
+    ).iterate():
+        counter += 1
+        print(message)
