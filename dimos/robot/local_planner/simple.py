ba2807a063ba5696b9a5d5d5d4eaeb7bfa3d8df3
lesh <lesh@sysphere.org>
2025-06-02 18:52:54 +0300

    random fiddling

--- dimos/robot/local_planner/simple.py
+++ dimos/robot/local_planner/simple.py
@@ -26,7 +26,7 @@ class SimplePlanner(LocalPlanner):
     goal: Optional[Vector] = None
     path: Optional[Path] = None
     speed: float = 0.5
-    slowdown_range = 0.3
+    slowdown_range = 0.5
 
     def set_path(self, path: Path) -> bool:
         self.path = path
@@ -35,6 +35,7 @@ class SimplePlanner(LocalPlanner):
 
     def set_goal(self, goal: VectorLike, stop_event=None, goal_theta=None) -> bool:
         self.goal = to_vector(goal).to_2d()
+        self.vis("local_goal", self.goal)
         logger.info(f"Setting goal: {self.goal}")
 
     def speed_ctrl(self, direction: Vector):
@@ -46,7 +47,7 @@ class SimplePlanner(LocalPlanner):
 
         return self.speed * f
 
-    def yaw_ctrl(self, direction, k_p: float = 1.0, omega_max: float = 0.2):
+    def yaw_ctrl(self, direction, k_p: float = 0.3, omega_max: float = 1):
         alpha = math.atan2(-direction.y, direction.x)
         omega = max(-omega_max, min(omega_max, k_p * alpha))
         return Vector(0.0, 0.0, omega)
@@ -59,23 +60,31 @@ class SimplePlanner(LocalPlanner):
     def filter(self, direction):
         length = direction.length()
         if self.path:
-            if length < 0.4:
+            if length < 0.5:
                 self.pop_path()
                 return False
         else:
-            if length < 0.1:
+            if length < 0.2:
                 return False
 
         return direction
 
-    def get_move_stream(self, frequency: float = 40.0) -> rx.Observable:
+    def ctrl(self, direction):
+        jaw = self.yaw_ctrl(direction)
+        if jaw.length() > 0.25:
+            return jaw
+        else:
+            return (direction.normalize() * self.speed_ctrl(direction)) + jaw
+
+    def get_move_stream(self, frequency: float = 30.0) -> rx.Observable:
         return rx.interval(1.0 / frequency, scheduler=get_scheduler()).pipe(
             ops.filter(lambda _: (self.goal is not None) or (self.path is not None)),
             ops.map(lambda _: self.get_robot_pos()),
             ops.map(lambda odom: odom.vector_to(self.goal)),
             ops.filter(self.filter),
+            ops.map(self.ctrl),
             # somewhere here we should detect potential obstacles in direction of travel
-            ops.map(lambda direction: direction.normalize() * self.speed_ctrl(direction)),
-            ops.map(lambda direction: direction),  # + self.yaw_ctrl(direction)),
+            #            ops.map(lambda direction: direction.normalize() * self.speed_ctrl(direction)),
+            #            ops.map(lambda direction: direction + self.yaw_ctrl(direction)),
             ops.map(lambda direction: Vector(-direction.y, direction.x, direction.z)),
         )
