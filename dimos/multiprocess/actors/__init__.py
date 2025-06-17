20cd5b9f6ce6ffa2a4191fae27e1a17b410b1538
lesh <lesh@sysphere.org>
2025-06-17 14:11:27 -0700

    Merge branch 'pyenv' of github.com:dimensionalOS/dimos into pyenv

--- dimos/multiprocess/actors/__init__.py
+++
@@ -1,7 +0,0 @@
-from dimos.multiprocess.actors.base import deploy_actor
-from dimos.multiprocess.actors.camera import CameraActor
-from dimos.multiprocess.actors.frame import LatencyActor
-from dimos.multiprocess.actors.recognition import FaceRecognitionActor
-from dimos.multiprocess.actors.video import VideoActor
-
-__all__ = ["CameraActor", "VideoActor", "LatencyActor", "FaceRecognitionActor", "deploy_actor"]
