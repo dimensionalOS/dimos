7bdca83b1e263158814c2460de27f561c09b4409
alexlin2 <alex.lin416@outlook.com>
2025-08-25 20:59:44 -0400

    remove the pynput based keyboard twist

--- dimos/robot/unitree_webrtc/unitree_go2.py
+++ dimos/robot/unitree_webrtc/unitree_go2.py
@@ -53,7 +53,6 @@ from dimos.robot.unitree_webrtc.type.map import Map
 from dimos.robot.unitree_webrtc.type.odometry import Odometry
 from dimos.robot.unitree_webrtc.unitree_skills import MyUnitreeSkills
 from dimos.robot.unitree_webrtc.depth_module import DepthModule
-from dimos.robot.keyboard_teleop import KeyboardTeleop
 from dimos.skills.skills import AbstractRobotSkill, SkillLibrary
 from dimos.utils.data import get_data
 from dimos.utils.logging_config import setup_logger
@@ -342,7 +341,6 @@ class UnitreeGo2(Robot):
         self.spatial_memory_module = None
         self.depth_module = None
         self.object_tracker = None
-        self.keyboard_teleop = None
 
         self._setup_directories()
 
@@ -375,7 +373,6 @@ class UnitreeGo2(Robot):
         self._deploy_visualization()
         self._deploy_perception()
         self._deploy_camera()
-        self._deploy_keyboard_teleop()
 
         self._start_modules()
 
@@ -522,15 +519,6 @@ class UnitreeGo2(Robot):
             self.object_tracker.camera_info.connect(self.connection.camera_info)
             logger.info("Object tracker connected to camera module")
 
-    def _deploy_keyboard_teleop(self):
-        """Deploy and configure the keyboard teleoperation module."""
-        self.keyboard_teleop = self.dimos.deploy(KeyboardTeleop)
-
-        # Connect keyboard output to robot movement command
-        self.keyboard_teleop.movecmd.transport = core.LCMTransport("/cmd_vel", Twist)
-
-        logger.info("Keyboard teleoperation module deployed and connected")
-
     def _start_modules(self):
         """Start all deployed modules in the correct order."""
         self.connection.start()
@@ -544,7 +532,6 @@ class UnitreeGo2(Robot):
         self.spatial_memory_module.start()
         self.depth_module.start()
         self.object_tracker.start()
-        self.keyboard_teleop.start()
 
         # Initialize skills after connection is established
         if self.skill_library is not None:
