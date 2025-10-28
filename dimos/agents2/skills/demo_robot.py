b2d70cb9533496b0b6efd949fa14edde36df24b5
Paul Nechifor <paul@nechifor.net>
2025-10-28 04:44:06 +0200

    update more to remove robot

--- dimos/agents2/skills/demo_robot.py
+++ dimos/agents2/skills/demo_robot.py
@@ -13,21 +13,28 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from dotenv import load_dotenv
+from reactivex import interval
 
-from dimos.agents2.agent import llm_agent
-from dimos.agents2.cli.human import human_input
-from dimos.agents2.skills.demo_robot import demo_robot
-from dimos.agents2.skills.osm import osm_skill
-from dimos.agents2.system_prompt import get_system_prompt
-from dimos.core.blueprints import autoconnect
-
-load_dotenv()
+from dimos.core.module import Module
+from dimos.core.stream import Out
+from dimos.mapping.types import LatLon
 
 
-demo_osm = autoconnect(
-    demo_robot(),
-    osm_skill(),
-    human_input(),
-    llm_agent(system_prompt=get_system_prompt()),
-)
+class DemoRobot(Module):
+    gps_location: Out[LatLon] = None
+
+    def start(self):
+        super().start()
+        self._disposables.add(interval(1.0).subscribe(lambda _: self._publish_gps_location()))
+
+    def stop(self):
+        super().stop()
+
+    def _publish_gps_location(self):
+        self.gps_location.publish(LatLon(lat=37.78092426217621, lon=-122.40682866540769))
+
+
+demo_robot = DemoRobot.blueprint
+
+
+__all__ = ["DemoRobot", "demo_robot"]
