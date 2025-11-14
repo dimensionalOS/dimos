389f0d26adc8c772d5f66dd9e31377d04281d127
Jeff Hykin <jeff.hykin@gmail.com>
2025-11-14 13:04:16 -0600

    provide alternative organization
    Former-commit-id: 8fcbfa29a97b7c8c039063203872d98b4eaa3698

--- flake.nix
+++ flake.nix
@@ -4,70 +4,113 @@
   inputs = {
     nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url  = "github:numtide/flake-utils";
+    lib.url          = "github:jeff-hykin/quick-nix-toolkits";
+    lib.inputs.flakeUtils.follows = "flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }:
+  outputs = { self, nixpkgs, flake-utils, lib, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
-
+        
         # ------------------------------------------------------------
         # 1. Shared package list (tool-chain + project deps)
         # ------------------------------------------------------------
-        devPackages = with pkgs; [
+        # we "flag" each package with what we need it for (e.g. LD_LIBRARY_PATH, nativeBuildInputs vs buildInputs, etc)
+        aggregation = lib.aggregator [
           ### Core shell & utils
-          bashInteractive coreutils gh
-          stdenv.cc.cc.lib pcre2
+          { vals.pkg=pkgs.bashInteractive;    flags={}; }
+          { vals.pkg=pkgs.coreutils;          flags={}; }
+          { vals.pkg=pkgs.gh;                 flags={}; }
+          { vals.pkg=pkgs.stdenv.cc.cc.lib;   flags={}; }
+          { vals.pkg=pkgs.pcre2;              flags.ldLibraryGroup=true; }
+          { vals.pkg=pkgs.git-lfs;            flags={}; }
+          { vals.pkg=pkgs.unixtools.ifconfig; flags={}; }
 
           ### Python + static analysis
-          python312 python312Packages.pip python312Packages.setuptools
-          python312Packages.virtualenv pre-commit
+          { vals.pkg=pkgs.python312;                    flags={}; }
+          { vals.pkg=pkgs.python312Packages.pip;        flags={}; }
+          { vals.pkg=pkgs.python312Packages.setuptools; flags={}; }
+          { vals.pkg=pkgs.python312Packages.virtualenv; flags={}; }
+          { vals.pkg=pkgs.pre-commit;                   flags={}; }
 
           ### Runtime deps
-          python312Packages.pyaudio portaudio ffmpeg_6 ffmpeg_6.dev
-
+          { vals.pkg=pkgs.python312Packages.pyaudio; flags={}; }
+          { vals.pkg=pkgs.portaudio;                 flags={}; }
+          { vals.pkg=pkgs.ffmpeg_6;                  flags={}; }
+          { vals.pkg=pkgs.ffmpeg_6.dev;              flags={}; }
+          
           ### Graphics / X11 stack
-          libGL libGLU mesa glfw
-          xorg.libX11 xorg.libXi xorg.libXext xorg.libXrandr xorg.libXinerama
-          xorg.libXcursor xorg.libXfixes xorg.libXrender xorg.libXdamage
-          xorg.libXcomposite xorg.libxcb xorg.libXScrnSaver xorg.libXxf86vm
-
-          udev SDL2 SDL2.dev zlib
+          { vals.pkg=pkgs.libGL;              flags.ldLibraryGroup=true; onlyIf=pkgs.stdenv.isLinux; }
+          { vals.pkg=pkgs.libGLU;             flags.ldLibraryGroup=true; onlyIf=pkgs.stdenv.isLinux; }
+          { vals.pkg=pkgs.mesa;               flags.ldLibraryGroup=true; onlyIf=pkgs.stdenv.isLinux; }
+          { vals.pkg=pkgs.glfw;               flags.ldLibraryGroup=true; onlyIf=pkgs.stdenv.isLinux; }
+          { vals.pkg=pkgs.xorg.libX11;        flags.ldLibraryGroup=true; onlyIf=pkgs.stdenv.isLinux; }
+          { vals.pkg=pkgs.xorg.libXi;         flags.ldLibraryGroup=true; onlyIf=pkgs.stdenv.isLinux; }
+          { vals.pkg=pkgs.xorg.libXext;       flags.ldLibraryGroup=true; onlyIf=pkgs.stdenv.isLinux; }
+          { vals.pkg=pkgs.xorg.libXrandr;     flags.ldLibraryGroup=true; onlyIf=pkgs.stdenv.isLinux; }
+          { vals.pkg=pkgs.xorg.libXinerama;   flags.ldLibraryGroup=true; onlyIf=pkgs.stdenv.isLinux; }
+          { vals.pkg=pkgs.xorg.libXcursor;    flags.ldLibraryGroup=true; onlyIf=pkgs.stdenv.isLinux; }
+          { vals.pkg=pkgs.xorg.libXfixes;     flags.ldLibraryGroup=true; onlyIf=pkgs.stdenv.isLinux; }
+          { vals.pkg=pkgs.xorg.libXrender;    flags.ldLibraryGroup=true; onlyIf=pkgs.stdenv.isLinux; }
+          { vals.pkg=pkgs.xorg.libXdamage;    flags.ldLibraryGroup=true; onlyIf=pkgs.stdenv.isLinux; }
+          { vals.pkg=pkgs.xorg.libXcomposite; flags.ldLibraryGroup=true; onlyIf=pkgs.stdenv.isLinux; }
+          { vals.pkg=pkgs.xorg.libxcb;        flags.ldLibraryGroup=true; onlyIf=pkgs.stdenv.isLinux; }
+          { vals.pkg=pkgs.xorg.libXScrnSaver; flags.ldLibraryGroup=true; onlyIf=pkgs.stdenv.isLinux; }
+          { vals.pkg=pkgs.xorg.libXxf86vm;    flags.ldLibraryGroup=true; onlyIf=pkgs.stdenv.isLinux; }
+          { vals.pkg=pkgs.udev;               flags.ldLibraryGroup=true; onlyIf=pkgs.stdenv.isLinux; }
+          { vals.pkg=pkgs.SDL2;               flags.ldLibraryGroup=true; onlyIf=pkgs.stdenv.isLinux; }
+          { vals.pkg=pkgs.SDL2.dev;           flags.ldLibraryGroup=true; onlyIf=pkgs.stdenv.isLinux; }
+          { vals.pkg=pkgs.zlib;               flags.ldLibraryGroup=true; onlyIf=pkgs.stdenv.isLinux; }
 
           ### GTK / OpenCV helpers
-          glib gtk3 gdk-pixbuf gobject-introspection
+          { vals.pkg=pkgs.glib;                  flags.ldLibraryGroup=true; onlyIf=pkgs.stdenv.isLinux; }
+          { vals.pkg=pkgs.gtk3;                  flags.ldLibraryGroup=true; onlyIf=pkgs.stdenv.isLinux; }
+          { vals.pkg=pkgs.gdk-pixbuf;            flags.ldLibraryGroup=true; onlyIf=pkgs.stdenv.isLinux; }
+          { vals.pkg=pkgs.gobject-introspection; flags.ldLibraryGroup=true; onlyIf=pkgs.stdenv.isLinux; }
           
           ### GStreamer
-          gst_all_1.gstreamer gst_all_1.gst-plugins-base gst_all_1.gst-plugins-good
-          gst_all_1.gst-plugins-bad gst_all_1.gst-plugins-ugly
-          python312Packages.gst-python
+          { vals.pkg=pkgs.gst_all_1.gstreamer;          flags.ldLibraryGroup=true; flags.giTypelibGroup=true; }
+          { vals.pkg=pkgs.gst_all_1.gst-plugins-base;   flags.ldLibraryGroup=true; flags.giTypelibGroup=true; }
+          { vals.pkg=pkgs.gst_all_1.gst-plugins-good;   flags={}; onlyIf=pkgs.stdenv.isLinux; }
+          { vals.pkg=pkgs.gst_all_1.gst-plugins-bad;    flags={}; onlyIf=pkgs.stdenv.isLinux; }
+          { vals.pkg=pkgs.gst_all_1.gst-plugins-ugly;   flags={}; onlyIf=pkgs.stdenv.isLinux; }
+          { vals.pkg=pkgs.python312Packages.gst-python; flags={}; onlyIf=pkgs.stdenv.isLinux; }
 
           ### Open3D & build-time
-          eigen cmake ninja jsoncpp libjpeg libpng
+          { vals.pkg=pkgs.eigen;   flags={}; }
+          { vals.pkg=pkgs.cmake;   flags={}; }
+          { vals.pkg=pkgs.ninja;   flags={}; }
+          { vals.pkg=pkgs.jsoncpp; flags={}; }
+          { vals.pkg=pkgs.libjpeg; flags={}; }
+          { vals.pkg=pkgs.libpng;  flags={}; }
           
           ### LCM (Lightweight Communications and Marshalling)
-          lcm
+          { vals.pkg=pkgs.lcm; flags.ldLibraryGroup=true; }
         ];
+        
+        # ------------------------------------------------------------
+        # 2. group / aggregate our packages
+        # ------------------------------------------------------------
+        devPackages = aggregation.getAll { attrPath=[ "pkg" ]; };
+        ldLibraryPackages = aggregation.getAll { hasAllFlags=[ "ldLibraryGroup" ]; attrPath=[ "pkg" ]; };
+        giTypelibPackagesString = aggregation.getAll {
+          hasAllFlags=[ "giTypelibGroup" ];
+          attrPath=[ "pkg" ];
+          strAppend="/lib/girepository-1.0";
+          strJoin=":"; 
+        };
+        ldGroupAndStdlib = [ pkgs.stdenv.cc.cc.lib ] ++ ldLibraryPackages;
 
         # ------------------------------------------------------------
-        # 2. Host interactive shell  →  `nix develop`
+        # 3. Host interactive shell  →  `nix develop`
         # ------------------------------------------------------------
         devShell = pkgs.mkShell {
           packages = devPackages;
           shellHook = ''
-            export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath [
-              pkgs.stdenv.cc.cc.lib pkgs.libGL pkgs.libGLU pkgs.mesa pkgs.glfw
-              pkgs.xorg.libX11 pkgs.xorg.libXi pkgs.xorg.libXext pkgs.xorg.libXrandr
-              pkgs.xorg.libXinerama pkgs.xorg.libXcursor pkgs.xorg.libXfixes
-              pkgs.xorg.libXrender pkgs.xorg.libXdamage pkgs.xorg.libXcomposite
-              pkgs.xorg.libxcb pkgs.xorg.libXScrnSaver pkgs.xorg.libXxf86vm
-              pkgs.udev pkgs.portaudio pkgs.SDL2.dev pkgs.zlib pkgs.glib pkgs.gtk3
-              pkgs.gdk-pixbuf pkgs.gobject-introspection pkgs.lcm pkgs.pcre2
-              pkgs.gst_all_1.gstreamer pkgs.gst_all_1.gst-plugins-base]}:$LD_LIBRARY_PATH"
-
+            export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath ldGroupAndStdlib}:$LD_LIBRARY_PATH"
             export DISPLAY=:0
-            export GI_TYPELIB_PATH="${pkgs.gst_all_1.gstreamer}/lib/girepository-1.0:${pkgs.gst_all_1.gst-plugins-base}/lib/girepository-1.0:$GI_TYPELIB_PATH"
-
+            export GI_TYPELIB_PATH="${giTypelibPackagesString}:$GI_TYPELIB_PATH" 
             PROJECT_ROOT=$(git rev-parse --show-toplevel 2>/dev/null || echo "$PWD")
             if [ -f "$PROJECT_ROOT/env/bin/activate" ]; then
               . "$PROJECT_ROOT/env/bin/activate"
