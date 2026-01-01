c689ad4644e3dc397da894dbb53fa2545cd1e9e9
Paul Nechifor <paul@nechifor.net>
2026-01-01 12:17:46 +0200

    Ignore individual errors (#919)
    * update ignores
    
    * latest errors
    
    * update years
    
    * update more years
    
    * update
    
    Former-commit-id: 94ead6da7b4c2e1dc21b5ee98e5379d8d91ec5b6

--- dimos/robot/unitree_webrtc/unitree_skill_container.py
+++ dimos/robot/unitree_webrtc/unitree_skill_container.py
@@ -1,4 +1,4 @@
-# Copyright 2025 Dimensional Inc.
+# Copyright 2025-2026 Dimensional Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -20,7 +20,7 @@ import math
 import time
 from typing import TYPE_CHECKING
 
-from unitree_webrtc_connect.constants import RTC_TOPIC  # type: ignore[import-untyped]
+from unitree_webrtc_connect.constants import RTC_TOPIC
 
 from dimos.core.core import rpc
 from dimos.core.skill_module import SkillModule
