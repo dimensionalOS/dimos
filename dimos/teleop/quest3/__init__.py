97c4e0b6a7a78104412e9075b8979647b6f2ffa1
ruthwikdasyam <ruthwikdasyam@gmail.com>
2026-01-08 22:12:28 +0000

    Misc: Init updates
    Former-commit-id: 965c54b934da0280300f0bd21f7e47bbfa76f789
    Former-commit-id: abb568d0904d5b24e29f0a5f6c71ffc14b4f8daf

--- dimos/teleop/quest3/__init__.py
+++ dimos/teleop/quest3/__init__.py
@@ -1,6 +1,6 @@
 """Quest3 VR Teleoperation."""
 
-from dimos.teleop.quest3.teleop_module import Quest3TeleopConfig, Quest3TeleopModule
+from dimos.teleop.quest3.quest3_teleop_module import Quest3TeleopConfig, Quest3TeleopModule
 
 __all__ = [
     "Quest3TeleopConfig",
