be6cce5286c9d3db94472a625b8b2ca0e3800816
Sam Bull <git@sambull.org>
2026-03-16 17:50:00 +0000

    Update test_dimos.py

--- dimos/robot/cli/test_dimos.py
+++ dimos/robot/cli/test_dimos.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from typing import Literal
+
 from dimos.core.blueprints import autoconnect
 from dimos.core.module import Module, ModuleConfig
 from dimos.robot.cli.dimos import arg_help
