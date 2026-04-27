ca6875926c4da81350e10c0478200c4ca701a930
Jeff Hykin <jeff.hykin@gmail.com>
2026-04-27 12:58:45 -0700

    nav_stack rename

--- dimos/hardware/sensors/lidar/fastlio2/module.py
+++ dimos/hardware/sensors/lidar/fastlio2/module.py
@@ -60,7 +60,7 @@ from dimos.msgs.geometry_msgs.Transform import Transform
 from dimos.msgs.geometry_msgs.Vector3 import Vector3
 from dimos.msgs.nav_msgs.Odometry import Odometry
 from dimos.msgs.sensor_msgs.PointCloud2 import PointCloud2
-from dimos.navigation.smart_nav.frames import FRAME_BODY, FRAME_ODOM
+from dimos.navigation.nav_stack.frames import FRAME_BODY, FRAME_ODOM
 from dimos.spec import mapping, perception
 from dimos.utils.logging_config import setup_logger
 
