e4e9cf917945d33f5f7b8dae9cc99f911eef1399
Shreyas Raorane <raorane@seas.upenn.edu>
2026-05-10 00:25:53 -0400

    odometry needs timeseries to be created...

---
+++ dimos/robot/drone/type/odometry.py
@@ -0,0 +1,9 @@
+from typing import Literal, TypedDict
+
+from dimos.msgs.geometry_msgs.PoseStamped import PoseStamped
+from dimos.msgs.geometry_msgs.Quaternion import Quaternion
+from dimos.msgs.geometry_msgs.Vector3 import Vector3
+from dimos.robot.drone.type.timeseries import (
+    Timestamped,
+)
+from dimos.types.timestamped import to_timestamp
