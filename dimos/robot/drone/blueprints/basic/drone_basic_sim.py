10de38f3ebd8b8523447d517cab09901bdc33356
Shreyas Raorane <raorane@seas.upenn.edu>
2026-05-09 21:36:16 -0400

    File added -- inspired from unitree_g1_basic_sim and drone_basic. Uses ReplanningAStar (Same as g1) for planning -- yet to be tested

---
+++ dimos/robot/drone/blueprints/basic/drone_basic_sim.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+""" Basic Drone Sim Blueprint with sim connection and visualization"""
+
+from typing import TYPE_CHECKING
+
+from dimos.core.coordination.blueprints import autoconnect
+from dimos.core.global_config import global_config
+from dimos.navigation.replanning_a_star.module import ReplanningAStarPlanner
+from dimos.robot.drone.blueprints.primitive.drone_primitive_no_nav import (
+    drone_primitive_no_nav,
+)
+from dimos.robot.drone.sim import DroneSimConnection
+
+drone_basic_sim = autoconnect(
+    drone_primitive_no_nav,
+    DroneSimConnection.blueprint(),
+    ReplanningAStarPlanner.blueprint(),
+)
+
+__all__ = ["drone_basic_sim"]
