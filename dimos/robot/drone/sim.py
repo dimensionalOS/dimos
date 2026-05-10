14c1995dbed92fe06567d4a099deaebf4b0f6fdd
Shreyas Raorane <raorane@seas.upenn.edu>
2026-05-10 00:14:53 -0400

    Added the imports, making the required files -- MujocoConnection and SimOdometry

---
+++ dimos/robot/drone/sim.py
@@ -0,0 +1,25 @@
+import threading 
+from threading import Thread
+import time
+from typing import Any
+
+from pydantic import Field
+from reactivex.disposable import Disposable
+
+from dimos.constants import DEFAULT_THREAD_JOIN_TIMEOUT
+from dimos.core.core import rpc
+from dimos.core.module import ModuleConfig
+from dimos.core.stream import In, Out
+from dimos.msgs.geometry_msgs.PoseStamped import PoseStamped
+from dimos.msgs.geometry_msgs.Quaternion import Quaternion
+from dimos.msgs.geometry_msgs.Transform import Transform
+from dimos.msgs.geometry_msgs.Twist import Twist
+from dimos.msgs.geometry_msgs.Vector3 import Vector3
+from dimos.msgs.sensor_msgs.CameraInfo import CameraInfo
+from dimos.msgs.sensor_msgs.Image import Image
+from dimos.msgs.sensor_msgs.PointCloud2 import PointCloud2
+
+from dimos.robot.drone.connection_module import DroneConnectionModule
+from dimos.robot.drone.mujoco_connection import MujocoConnection
+from dimos.robot.drone.type.odometry import Odometry as SimOdometry
+from dimos.utils.logging_config import setup_logger
