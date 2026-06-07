5b287413401235b2edef998928ded0f2aae6a126
Shreyas Raorane <raorane@seas.upenn.edu>
2026-06-07 17:56:49 -0400

    keyboard teleop added to drone basic sim -- everything works!!!

--- dimos/robot/drone/blueprints/basic/drone_basic_sim.py
+++ dimos/robot/drone/blueprints/basic/drone_basic_sim.py
@@ -5,10 +5,12 @@
 from dimos.core.coordination.blueprints import autoconnect
 from dimos.robot.drone.blueprints.primitive.drone_primitive_no_nav import drone_primitive_no_nav
 from dimos.robot.drone.mujoco_sim import DroneSimConnection
+from dimos.robot.unitree.keyboard_teleop import KeyboardTeleop
 
 drone_basic_sim = autoconnect(
     drone_primitive_no_nav,
     DroneSimConnection.blueprint(),
+    KeyboardTeleop.blueprint(publish_only_when_active=True),
 )
 
 __all__ = ["drone_basic_sim"]
