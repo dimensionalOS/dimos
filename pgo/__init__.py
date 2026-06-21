70e56462f4587ba337b0f1aa880031618ae4a69b
Jeff Hykin <jeff.hykin@gmail.com>
2026-06-22 01:35:14 +0800

    clean

--- pgo/__init__.py
+++
@@ -1,1 +0,0 @@
-"""PGO post-processing package (relocated from jnav for the better_pgo branch)."""
--- pgo/utils/__init__.py
+++
@@ -1,1 +0,0 @@
-"""Helpers vendored from dimos3 jnav/utils (recording_db, trajectory_metrics, voxel_map)."""
