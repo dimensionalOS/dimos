bd5c754f1a9ef1763bb858b10e021bb66ccd4f98
Andrew Lauer <andrewcl@email.unc.edu>
2026-07-27 19:27:29 -0700

    CI fix

--- dimos/hardware/sensors/lidar/pointlio/cpp/flake.nix
+++ dimos/hardware/sensors/lidar/pointlio/cpp/flake.nix
@@ -18,9 +18,8 @@
       flake = false;
     };
     fast-lio = {
-      # Point-LIO fork (split out of dimos-module-fastlio2's pointlio branch).
-      # Repo is org-internal for now, hence git+ssh instead of github:.
-      url = "git+ssh://git@github.com/dimensionalOS/dimos-module-pointlio?ref=main";
+      # Point-LIO fork, split out of dimos-module-fastlio2's pointlio branch.
+      url = "github:dimensionalOS/dimos-module-pointlio?ref=main";
       flake = false;
     };
     lcm-extended = {
