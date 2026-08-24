5b527b46ba04b019af813ee3709c24ee89b9e3d1
Jeff Hykin <jeff.hykin@gmail.com>
2026-08-24 03:33:29 -0700

    cuvslam: trim two comments that restate the code

--- dimos/mapping/odometry_path.py
+++ dimos/mapping/odometry_path.py
@@ -12,8 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Accumulate an odometry stream into the path it has travelled."""
-
 from __future__ import annotations
 
 from collections import deque
