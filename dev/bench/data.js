23e5350ea458eec9a4e23757d42564edc9724716
github-action-benchmark <github@users.noreply.github.com>
2026-09-24 01:19:56 +0000

    add go2 replay realtime (customSmallerIsBetter) benchmark result for beaaff4865eb1be96ea6366862e59eb27b6270bb

---
+++ dev/bench/data.js
@@ -0,0 +1,92 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1790212796909,
+  "repoUrl": "https://github.com/dimensionalOS/dimos",
+  "entries": {
+    "go2 replay realtime": [
+      {
+        "commit": {
+          "author": {
+            "email": "git@sambull.org",
+            "name": "Sam Bull",
+            "username": "Dreamsorcerer"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "beaaff4865eb1be96ea6366862e59eb27b6270bb",
+          "message": "Benchmark replay (#3524)",
+          "timestamp": "2026-09-24T04:16:49+03:00",
+          "tree_id": "72e95df27401e1933ff298bcd8c44ebb3144e013",
+          "url": "https://github.com/dimensionalOS/dimos/commit/beaaff4865eb1be96ea6366862e59eb27b6270bb"
+        },
+        "date": 1790212795532,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "first frame wall",
+            "value": 10.201,
+            "unit": "s"
+          },
+          {
+            "name": "first frame cpu",
+            "value": 21.324,
+            "unit": "s"
+          },
+          {
+            "name": "run cpu",
+            "value": 30.497,
+            "unit": "s"
+          },
+          {
+            "name": "run cpu (user)",
+            "value": 22.006,
+            "unit": "s"
+          },
+          {
+            "name": "run cpu (system)",
+            "value": 8.491,
+            "unit": "s"
+          },
+          {
+            "name": "peak memory",
+            "value": 2707.664,
+            "unit": "MB"
+          },
+          {
+            "name": "peak threads",
+            "value": 396,
+            "unit": "threads"
+          },
+          {
+            "name": "disk read",
+            "value": 24.402,
+            "unit": "MB"
+          },
+          {
+            "name": "disk write",
+            "value": 810.344,
+            "unit": "MB"
+          },
+          {
+            "name": "network (multicast)",
+            "value": 0.003,
+            "unit": "MB"
+          },
+          {
+            "name": "network (external rx)",
+            "value": 0.1,
+            "unit": "MB"
+          },
+          {
+            "name": "network (external tx)",
+            "value": 0.141,
+            "unit": "MB"
+          }
+        ]
+      }
+    ]
+  }
+}
