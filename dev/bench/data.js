9a418ea468396a91d5b82c813355e72093b16219
github-action-benchmark <github@users.noreply.github.com>
2026-09-25 21:48:48 +0000

    add go2 replay realtime (customSmallerIsBetter) benchmark result for 9b2e7c95eeebfece1ab77b7d37bce50206d40a77

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1790372833784,
+  "lastUpdate": 1790372928019,
   "repoUrl": "https://github.com/dimensionalOS/dimos",
   "entries": {
     "go2 replay realtime": [
@@ -335,6 +335,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "network (external tx)",
             "value": 0.147,
+            "unit": "MB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "paul@nechifor.net",
+            "name": "Paul Nechifor",
+            "username": "paul-nechifor"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "9b2e7c95eeebfece1ab77b7d37bce50206d40a77",
+          "message": "remove network benchmark (#4288)",
+          "timestamp": "2026-09-25T21:45:45Z",
+          "tree_id": "baef47dfdaa44693a326c3fde9f57e11ac8ce954",
+          "url": "https://github.com/dimensionalOS/dimos/commit/9b2e7c95eeebfece1ab77b7d37bce50206d40a77"
+        },
+        "date": 1790372927150,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "first frame wall",
+            "value": 9.853,
+            "unit": "s"
+          },
+          {
+            "name": "first frame cpu",
+            "value": 20.246,
+            "unit": "s"
+          },
+          {
+            "name": "run cpu",
+            "value": 35.605,
+            "unit": "s"
+          },
+          {
+            "name": "run cpu (user)",
+            "value": 26.839,
+            "unit": "s"
+          },
+          {
+            "name": "run cpu (system)",
+            "value": 8.765,
+            "unit": "s"
+          },
+          {
+            "name": "peak memory",
+            "value": 2707.773,
+            "unit": "MB"
+          },
+          {
+            "name": "peak threads",
+            "value": 404,
+            "unit": "threads"
+          },
+          {
+            "name": "disk read",
+            "value": 13.238,
+            "unit": "MB"
+          },
+          {
+            "name": "disk write",
+            "value": 251.051,
             "unit": "MB"
           }
         ]
